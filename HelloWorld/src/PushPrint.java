e3a2f20c3528311b7c1c6e0e492ebea72086a2fb
kkeonu <kkeonu@kakao.com>
2023-04-05 09:59:04 +0900

    save

--- HelloWorld/src/PushPrint.java
+++ HelloWorld/src/PushPrint.java
@@ -1,6 +1,6 @@
 
 public class PushPrint {
-	public static void main(string[] args) {
+	public static void main(String[] args) {
 		System.out.println("Add Java File");
 		System.out.println("Hello in Local...");
 	}
